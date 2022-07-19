2a155008ba325cb48ebaaacc115498c63dafbcb9
Grazi <diehlgraziela@gmail.com>
2022-07-19 13:53:31 -0300

    gallery javascript

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,34 @@ for (link of navLinks) {
         }
     })
 }
+
+//Gallery
+(function () {
+    let filterOption = document.querySelectorAll('.filter-option');
+    let galleryImage = document.querySelectorAll('.gallery-image');
+
+    filterOption.forEach((option) => {
+        option.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            document.querySelector(".filter-options .active").classList.remove("active");
+            e.target.classList.add("active");
+
+            let filter = e.target.dataset.filter;
+
+            galleryImage.forEach((item) => {
+                if (filter === 'all') {
+                    item.style.display = 'block';
+                } else {
+
+                    if (item.classList.contains(filter)) {
+                        item.style.display = 'block';
+                    } else {
+                        item.style.display = 'none';
+                    }
+                }
+            })
+        })
+    })
+
+})();
